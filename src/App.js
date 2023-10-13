cd6a01e7a62e9432abe279b0bc108c26d4820c2f
TahaZairig <tahazairig1@gmail.com>
2023-10-13 19:14:30 +0100

    initializing react with tailwind and iconscout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl font-bold text-red-500'>Hello, World</h1>
+      <UilReact size={50} />
+    </div>
+  );
+}
+
+export default App;
